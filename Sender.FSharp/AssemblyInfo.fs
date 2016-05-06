ad254627bb1ad98d1a933ea3fc1747cc3b9dab84
mavnn <michael@mavnn.co.uk>
2016-05-06 21:59:40 +0100

    C# complete

---
+++ Sender.FSharp/AssemblyInfo.fs
@@ -0,0 +1,20 @@
+﻿namespace Sender.FSharp
+open System.Reflection
+open System.Runtime.CompilerServices
+
+[<assembly: AssemblyTitle("Sender.FSharp")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("")>]
+[<assembly: AssemblyCopyright("mavnn")>]
+[<assembly: AssemblyTrademark("")>]
+
+// The assembly version has the format {Major}.{Minor}.{Build}.{Revision}
+
+[<assembly: AssemblyVersion("1.0.0.0")>]
+
+//[<assembly: AssemblyDelaySign(false)>]
+//[<assembly: AssemblyKeyFile("")>]
+
+()
